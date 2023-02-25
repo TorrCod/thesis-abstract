5f925bb6367c2807352ac6fac121fe218ba4c72e
torrcod <torreschristian811@gmail.com>
2023-02-25 19:33:15 +0800

    Minor changes in style

--- src/pages/thesis.tsx
+++ src/pages/thesis.tsx
@@ -27,11 +27,11 @@ const Thesis = () => {
 
 const Items = () => {
   return (
-    <div className="thesis_items w-full bg-slate-100 md:h-82 h-auto max-w-[50em] shadow-md rounded-md p-5 gap-5 grid">
-      <div className="div1 grid place-items-center bg-white shadow-md rounded-sm max-w-xs min-h-[25em] md:min-h-[20] place-self-center w-full">
+    <div className="thesis_items w-full bg-slate-100 md:h-82 h-auto max-w-[50em] shadow-md rounded-md p-5 gap-2 md:gap-5 grid">
+      <div className="div1 grid place-items-center bg-white shadow-md rounded-sm max-w-xs min-h-[25em] md:min-h-[20em] place-self-center w-full">
         preview
       </div>
-      <div className="div2 flex flex-col py-5 gap-5">
+      <div className="div2 flex flex-col md:py-5 md:gap-5 gap-2">
         <div>
           <span className="text-sm text-[#38649C]">Title</span>
           <h2>
@@ -48,7 +48,7 @@ const Items = () => {
           <h2>01/01/2023</h2>
         </div>
       </div>
-      <div className="div3 p-5">
+      <div className="div3 md:p-5 px-5">
         <div>
           <span className="text-sm text-[#38649C]">Researchers</span>
           <ul className="list-disc">

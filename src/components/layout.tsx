238954a4eeb1905dbdd25f4b0ce2e4676f4bebbc
torrcod <torreschristian811@gmail.com>
2023-02-28 00:51:47 +0800

    dynamic pages for dummy data done

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import { GlobalWrapper } from "@/context/globalContext";
 import AntD_Config from "@/styles/antd_config";
 import Background from "./background";
 import NavBar from "./navbar";
@@ -5,11 +6,13 @@ import NavBar from "./navbar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      <AntD_Config>
-        {/* <Background /> */}
-        <NavBar />
-        {children}
-      </AntD_Config>
+      <GlobalWrapper>
+        <AntD_Config>
+          {/* <Background /> */}
+          <NavBar />
+          {children}
+        </AntD_Config>
+      </GlobalWrapper>
     </main>
   );
 };
--- src/context/globalContext.tsx
+++ src/context/globalContext.tsx
@@ -1,5 +1,23 @@
-const globalContext = () => {
-  return <></>;
+import { createContext, useContext } from "react";
+import { GlobalState, ThesisItems } from "./types.d";
+import thesisData from "@/data/data.json";
+
+const globalContext_Init: GlobalState = {
+  thesisItems: thesisData as unknown as ThesisItems[],
 };
 
-export default globalContext;
+const GlobalContext = createContext<GlobalState>(globalContext_Init);
+
+export const GlobalWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <GlobalContext.Provider value={globalContext_Init}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
+
+export default useGlobalContext;

69de40224d7e0319a700bdcbb8f4ada0eb1ef2e5
Christian Bert Torres <105502630+TorrCod@users.noreply.github.com>
2023-02-27 22:25:08 +0800

    Matsu sign up (#2)
    * First Commit
    
    Co-authored-by: Christian Bert Torres <TorrCod@users.noreply.github.com>
    
    * Update Test
    
    * Update from  before, will continue coding rn
    
    * Added SignIn SignUp System
    
    ---------
    
    Co-authored-by: Matsumitsu03 <matsumitsu173@gmail.com>
    Co-authored-by: Christian Bert Torres <TorrCod@users.noreply.github.com>

--- src/components/signin_signup.tsx
+++ src/components/signin_signup.tsx
@@ -1,7 +1,123 @@
-import { PriButton } from "./button";
+import { useState } from 'react';
+import { Modal, Form, Input, Button } from 'antd';
 
-function Login() {
-  return <PriButton>Signin / Sign Up</PriButton>;
-}
+const SignInSignUp = () => {
+  const [open, setOpen] = useState(false);
+  const [form] = Form.useForm();
 
-export default Login;
+  const handleOk = async () => {
+    try {
+      await form.validateFields();
+      // TODO: handle sign in/signup logic
+      setOpen(false); // close the modal after successful sign in/signup
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#1890ff',
+    borderColor: '#1890ff',
+    color: '#fff',
+    borderRadius: '4px',
+    marginRight: '10px',
+    marginBottom: '10px',
+    fontSize: '16px',
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Sign In/Sign Up
+      </Button>
+      <Modal open={open} onCancel={handleCancel} footer={null}>
+        <h2>Sign In/Sign Up</h2>
+        <Form form={form} layout="vertical">
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please enter your email' }]}
+          >
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please enter your password' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item>
+            <Button style={buttonStyle} type="primary" onClick={handleOk}>
+              Sign In
+            </Button>
+          </Form.Item>
+          <hr />
+          <Form.Item
+            label="First Name"
+            name="firstName"
+            rules={[{ required: true, message: 'Please enter your first name' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Last Name"
+            name="lastName"
+            rules={[{ required: true, message: 'Please enter your last name' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="signupEmail"
+            rules={[{ required: true, message: 'Please enter your email' }]}
+          >
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="signupPassword"
+            rules={[{ required: true, message: 'Please enter your password' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Confirm Password"
+            name="confirmPassword"
+            dependencies={['signupPassword']}
+            rules={[
+              { required: true, message: 'Please confirm your password' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('signupPassword') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error('The two passwords do not match')
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item>
+            <Button style={buttonStyle} type="primary" onClick={handleOk}>
+              Sign Up
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+};
+
+export default SignInSignUp;

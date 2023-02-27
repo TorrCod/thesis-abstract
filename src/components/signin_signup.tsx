25cb8d525204baf2138c35bcf91d2be745fd509d
Matsumitsu03 <matsumitsu173@gmail.com>
2023-02-27 17:57:08 +0800

    Update from  before, will continue coding rn

--- src/components/signin_signup.tsx
+++ src/components/signin_signup.tsx
@@ -1,7 +1,20 @@
+import { Form, Modal } from "antd";
+import { useState } from "react";
 import { PriButton } from "./button";
 
 function Login() {
-  return <PriButton>SigninS</PriButton>;
+  const [openModal, setOpenModal] = useState(false)
+
+  return (
+    <>
+      <PriButton onClick={()=>setOpenModal(true)}>Signin / Sign Up</PriButton>
+      <Modal open={openModal} onCancel={()=>setOpenModal(false)} onOk={()=>{}}>
+        <Form>
+          SignIn Mamaya na lang uli
+        </Form>
+      </Modal>
+    </>
+  );
 }
 
 export default Login;

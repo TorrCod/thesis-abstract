ef80c56e46c4fd5bf2b0fbf5d0bd11f281acc676
torrcod <torreschristian811@gmail.com>
2023-02-28 16:40:34 +0800

    shadow added on button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,7 @@
 import { Button, ButtonProps } from "antd";
 
 export const PriButton = (Props: ButtonProps) => {
-  return <Button className="bg-[#F8B49C]" type="primary" {...Props} />;
+  return (
+    <Button className="bg-[#F8B49C] shadow-lg" type="primary" {...Props} />
+  );
 };

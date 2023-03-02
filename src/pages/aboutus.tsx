32a72e1072c578c5dfee28cc55efb0a90ea5f4b7
torrcod <torreschristian811@gmail.com>
2023-03-02 13:57:19 +0800

    wrapped section component on Whole AboutUs

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,27 +1,24 @@
-import { Typography, Row, Divider, Button, Modal, Form, Input } from 'antd';
-import React, { useState } from 'react';
+import { Typography, Row, Divider, Button, Modal, Form, Input } from "antd";
+import React, { useState } from "react";
 
 const { Title, Paragraph } = Typography;
 
 const styles = {
-
   title: {
-    fontSize: '48px',
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: '16px',
-    marginTop: '50px',
+    fontSize: "48px",
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: "16px",
+    marginTop: "50px",
   },
 
   paragraph: {
-    fontSize: '24px',
-    color: '#666',
-    marginBottom: '32px',
-    maxWidth: '600px',
+    fontSize: "24px",
+    color: "#666",
+    marginBottom: "32px",
+    maxWidth: "600px",
   },
-
-}
-
+};
 
 interface FormValues {
   name: string;
@@ -45,7 +42,7 @@ const ContactUs = () => {
   };
 
   const onFinish = (values: FormValues) => {
-    console.log('Received values of form: ', values);
+    console.log("Received values of form: ", values);
   };
 
   return (
@@ -53,23 +50,31 @@ const ContactUs = () => {
       <Button type="primary" onClick={showModal}>
         Contact Us
       </Button>
-      <Modal title="Contact Us" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Modal
+        title="Contact Us"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
         <Form name="contact" onFinish={onFinish}>
-          <Form.Item name="name" rules={[{ required: true, message: 'Please enter your name' }]}>
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
             <Input placeholder="Name" />
           </Form.Item>
           <Form.Item
             name="email"
             rules={[
-              { required: true, message: 'Please enter your email' },
-              { type: 'email', message: 'Please enter a valid email' },
+              { required: true, message: "Please enter your email" },
+              { type: "email", message: "Please enter a valid email" },
             ]}
           >
             <Input placeholder="Email" />
           </Form.Item>
           <Form.Item
             name="message"
-            rules={[{ required: true, message: 'Please enter your message' }]}
+            rules={[{ required: true, message: "Please enter your message" }]}
           >
             <Input.TextArea placeholder="Message" />
           </Form.Item>
@@ -82,26 +87,30 @@ const ContactUs = () => {
       </Modal>
     </>
   );
-}
-
+};
 
 const AboutUs = () => {
   return (
-    <>
+    <section>
       <header>
-        <Divider orientation="center" >
-          <Title level={4} style={styles.title}>About Us</Title>
+        <Divider orientation="center">
+          <Title level={4} style={styles.title}>
+            About Us
+          </Title>
         </Divider>
       </header>
       <Row gutter={[16, 16]} justify="center">
         <Paragraph style={styles.paragraph}>
-          Welcome to our Thesis Abstract Management System for the College of Engineering in Morong Rizal! We are a team of undergraduate
-          computer engineering students from the University of Rizal System in Morong Rizal, Philippines. Our goal is to provide a
-          user-friendly and efficient system for managing thesis abstract submissions for the College of Engineering.
+          Welcome to our Thesis Abstract Management System for the College of
+          Engineering in Morong Rizal! We are a team of undergraduate computer
+          engineering students from the University of Rizal System in Morong
+          Rizal, Philippines. Our goal is to provide a user-friendly and
+          efficient system for managing thesis abstract submissions for the
+          College of Engineering.
         </Paragraph>
       </Row>
       <ContactUs />
-    </>
+    </section>
   );
 };
 

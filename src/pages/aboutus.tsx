6e48a044f3cb0b799c0ac4c0652c9b4d7dee5797
torrcod <torreschristian811@gmail.com>
2023-03-03 23:09:16 +0800

    about us initialize

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,168 +1,149 @@
-import { Typography, Row, Divider, Button, Modal, Form, Input } from "antd";
-import React, { useState } from "react";
-
-const { Title, Paragraph } = Typography;
-
-const styles = {
-  title: {
-    fontSize: "48px",
-    fontWeight: "bold",
-    color: "#53326",
-    marginBottom: "16px",
-  },
-
-  paragraph: {
-    fontSize: "14px",
-    color: "black",
-    marginBottom: "32px",
-    maxWidth: "600px",
-  },
-
-  contactbutton: {
-    alignitems: "flex",
-    margin: "20px",
-    background: "aqua",
-    marginRight: "40px",
-    display: "flex",
-    marginLeft: "auto",
-  },
-};
-
-interface FormValues {
-  name: string;
-  email: string;
-  message: string;
-}
-
-const ContactUs = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  const onFinish = (values: FormValues) => {
-    console.log("Received values of form: ", values);
-  };
-
-  return (
-    <>
-      <Button type="primary" onClick={showModal} style={styles.contactbutton}>
-        Contact Us
-      </Button>
-      <Modal
-        title="Contact Us"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Form name="contact" onFinish={onFinish}>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: "Please enter your name" }]}
-          >
-            <Input placeholder="Name" />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            rules={[
-              { required: true, message: "Please enter your email" },
-              { type: "email", message: "Please enter a valid email" },
-            ]}
-          >
-            <Input placeholder="Email" />
-          </Form.Item>
-          <Form.Item
-            name="message"
-            rules={[{ required: true, message: "Please enter your message" }]}
-          >
-            <Input.TextArea placeholder="Message" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </>
-  );
-};
+import { PriButton } from "@/components/button";
+import { Avatar, Card, Divider, Form, Input } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import Image from "next/image";
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { CiLocationOn } from "react-icons/ci";
 
 const AboutUs = () => {
   return (
-    <section className="relative md:pt-20">
-      <header>
-        <Title className="text-center" level={4} style={styles.title}>
-          About Us
-        </Title>
-      </header>
-      <Row gutter={[16, 16]} justify="center">
-        <Paragraph style={styles.paragraph}>
-          <p>
-            Welcome to our Thesis Abstract Management System for the College of
-            Engineering in Morong Rizal! We are a team of undergraduate computer
-            engineering students from the University of Rizal System in Morong
-            Rizal, Philippines. Our goal is to provide a user-friendly and
-            efficient system for managing thesis abstract submissions for the
-            College of Engineering.
-          </p>
-          <p>
-            Our mission is to streamline the thesis submission process for
-            students and faculty. We understand that submitting a thesis can be
-            a daunting and time-consuming task, which is why we developed our
-            Thesis Abstract Management System to simplify the process and reduce
-            errors.
-          </p>
-          <p>
-            The development of our Thesis Abstract Management System began as a
-            response to the challenges faced by students and faculty in managing
-            thesis abstract submissions. We are proud to have achieved several
-            milestones along the way, including the successful completion of the
-            system's prototype and beta testing.
-          </p>
-          <p>
-            Our team is composed of dedicated computer engineering students with
-            expertise in software development and programming. We are passionate
-            about creating innovative solutions to complex problems.
-          </p>
-          <p>
-            Our system's key features include a user-friendly interface, an
-            easy-to-use submission process, and a real-time tracking system.
-            These features allow students and faculty to submit and manage their
-            thesis abstracts with ease and transparency.
-          </p>
-          <p>
-            By using our system, students and faculty will benefit from improved
-            efficiency, reduced errors, and increased transparency. In addition,
-            our Thesis Abstract Management System also provides a platform for
-            real-time feedback and support.
-          </p>
-          <p>
-            Don't just take our word for it, here's what some of our users have
-            to say: [insert testimonials from users]
-          </p>
-          <p>
-            If you have any questions or need technical support, please don't
-            hesitate to contact our team. You can find our contact information
-            on our website.
-          </p>
-          <p>
-            We invite you to try our Thesis Abstract Management System for the
-            College of Engineering in Morong Rizal. Sign up today and experience
-            a streamlined and efficient thesis submission process.
-          </p>
-        </Paragraph>
-      </Row>
-      <ContactUs />
+    <section>
+      <div>
+        <div className="relative h-[15em] w-3/4 m-auto">
+          <Image
+            style={{ objectFit: "cover" }}
+            src={"/book.svg"}
+            alt="book"
+            fill
+          />
+        </div>
+        <h1 className="text-center text-lg">
+          THESIS ABSTRACT MANAGEMENT SYSTEM FOR COLLEGE OF ENGINEERING
+        </h1>
+        <p>
+          Welcome to our Thesis Abstract Management System for the College of
+          Engineering in Morong Rizal! We are a team of undergraduate computer
+          engineering students from the University of Rizal System in Morong
+          Rizal, Philippines. Our goal is to provide a user-friendly and
+          efficient system for managing thesis abstract submissions for the
+          College of Engineering.
+        </p>
+      </div>
+      <Divider />
+      <div>
+        <div className="relative h-[15em] w-3/4 m-auto">
+          <Image
+            style={{ objectFit: "cover" }}
+            src={"/goals.svg"}
+            alt="goal"
+            fill
+          />
+        </div>
+        <h1 className="text-center text-lg">WHAT GOALS WE INTENDED TO DO?</h1>
+        <p>
+          Our mission is to streamline the thesis submission process for
+          students and faculty. We understand that submitting a thesis can be a
+          daunting and timeconsuming task, which is why we developed our Thesis
+          Abstract Management System to simplify the process and reduce errors.
+        </p>
+      </div>
+      <Divider />
+      <div>
+        <div className="relative h-[15em] w-3/4 m-auto">
+          <Image
+            style={{ objectFit: "cover" }}
+            src={"/puzzle.svg"}
+            alt="puzzle"
+            fill
+          />
+        </div>
+        <p>
+          Welcome to our Thesis Abstract Management System for the College of
+          Engineering in Morong Rizal! We are a team of undergraduate computer
+          engineering students from the University of Rizal System in Morong
+          Rizal, Philippines. Our goal is to provide a user-friendly and
+          efficient system for managing thesis abstract submissions for the
+          College of Engineering.
+        </p>
+      </div>
+      <Divider />
+      <div>
+        <div>
+          By using our system, students and faculty will benefit from improved
+          efficiency, reduced errors, and increased transparency. In addition,
+          our Thesis Abstract Management System also provides a platform for
+          real-time feedback and support.
+        </div>
+        <div>
+          By using our system, students and faculty will benefit from improved
+          efficiency, reduced errors, and increased transparency. In addition,
+          our Thesis Abstract Management System also provides a platform for
+          real-time feedback and support.
+        </div>
+      </div>
+      <Divider />
+      <div>
+        <h1 className="text-lg">GET IN TOUCH</h1>
+        <p>
+          If you have any questions or need technical support, please don't
+          hesitate to contact our team. You can find our contact information on
+          our website.
+        </p>
+        <div>
+          <AiOutlinePhone />
+          torreschristian@gmail.com
+        </div>
+        <div>
+          <AiOutlineMail />
+          09683680391
+        </div>
+        <div>
+          <CiLocationOn />
+          URS MORONG
+        </div>
+        <Card title="Message Us">
+          <Form>
+            <Form.Item>
+              <Input placeholder="name" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="email" />
+            </Form.Item>
+            <Form.Item>
+              <TextArea placeholder="message... " />
+            </Form.Item>
+            <Form.Item>
+              <PriButton>SEND</PriButton>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+      <Divider />
+      <div>
+        <h1 className="text-center">RESEARCHER</h1>
+        <div>
+          <div>
+            <Avatar size={"large"} />
+            <div>Christian Bert Torres</div>
+          </div>
+          <div>
+            <Avatar size={"large"} />
+            <div>Christian Bert Torres</div>
+          </div>
+          <div>
+            <Avatar size={"large"} />
+            <div>Christian Bert Torres</div>
+          </div>
+          <div>
+            <Avatar size={"large"} />
+            <div>Christian Bert Torres</div>
+          </div>
+          <div>
+            <Avatar size={"large"} />
+            <div>Christian Bert Torres</div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };

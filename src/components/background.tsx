3f6e1a50cdaf9b28f9a67a97b67d561d04847317
torrcod <torreschristian811@gmail.com>
2023-03-04 15:02:24 +0800

    about us responsive

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -4,20 +4,7 @@ import Image from "next/image";
 const Background = () => {
   return (
     <div className="bg-circle">
-      <svg
-        viewBox="0 0 2123 1932"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <ellipse
-          cx="1088.5"
-          cy="1032.5"
-          rx="1034.5"
-          ry="899.5"
-          fill="#F8B49C"
-        />
-        <ellipse cx="1034.5" cy="899.5" rx="1034.5" ry="899.5" fill="#38649C" />
-      </svg>
+      <Image priority src="/bg-circles.svg" fill alt="Follow us on Twitter" />
     </div>
   );
 };

f44386f6241206bb37c9615e8bec246376910f7e
Christian Bert Torres <torreschristian811@gmail.com>
2023-03-04 15:12:57 +0800

    About us enhancement (#16)
    * about us initialize
    
    * bg fixed
    
    * header done
    
    * about us responsive
    
    * margin top fix
    
    * scroll fix in home page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,16 +6,17 @@ import NavBar from "./navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main>
-      <GlobalWrapper>
-        <UserWrapper>
-          <AntD_Config>
-            <NavBar />
+    <GlobalWrapper>
+      <UserWrapper>
+        <AntD_Config>
+          <NavBar />
+          <main>
+            <Background />
             {children}
-          </AntD_Config>
-        </UserWrapper>
-      </GlobalWrapper>
-    </main>
+          </main>
+        </AntD_Config>
+      </UserWrapper>
+    </GlobalWrapper>
   );
 };
 
--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,169 +1,170 @@
-import { Typography, Row, Divider, Button, Modal, Form, Input } from "antd";
-import React, { useState } from "react";
-
-const { Title, Paragraph } = Typography;
-
-const styles = {
-  title: {
-    fontSize: "48px",
-    fontWeight: "bold",
-    color: "#53326",
-    marginBottom: "16px",
-  },
-
-  paragraph: {
-    fontSize: "14px",
-    color: "black",
-    marginBottom: "32px",
-    maxWidth: "600px",
-  },
-
-  contactbutton: {
-    alignitems: "flex",
-    margin: "20px",
-    background: "aqua",
-    marginRight: "40px",
-    display: "flex",
-    marginLeft: "auto",
-  },
-};
-
-interface FormValues {
-  name: string;
-  email: string;
-  message: string;
-}
-
-const ContactUs = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  const onFinish = (values: FormValues) => {
-    console.log("Received values of form: ", values);
-  };
-
-  return (
-    <>
-      <Button type="primary" onClick={showModal} style={styles.contactbutton}>
-        Contact Us
-      </Button>
-      <Modal
-        title="Contact Us"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Form name="contact" onFinish={onFinish}>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: "Please enter your name" }]}
-          >
-            <Input placeholder="Name" />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            rules={[
-              { required: true, message: "Please enter your email" },
-              { type: "email", message: "Please enter a valid email" },
-            ]}
-          >
-            <Input placeholder="Email" />
-          </Form.Item>
-          <Form.Item
-            name="message"
-            rules={[{ required: true, message: "Please enter your message" }]}
-          >
-            <Input.TextArea placeholder="Message" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </>
-  );
-};
+import { PriButton } from "@/components/button";
+import { Avatar, Card, Divider, Form, Input } from "antd";
+import Image from "next/image";
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { IoLocationOutline } from "react-icons/io5";
 
 const AboutUs = () => {
   return (
-    <section className="relative md:pt-20">
-      <header>
-        <Title className="text-center" level={4} style={styles.title}>
-          About Us
-        </Title>
-      </header>
-      <Row gutter={[16, 16]} justify="center">
-        <Paragraph style={styles.paragraph}>
-          <p>
-            Welcome to our Thesis Abstract Management System for the College of
-            Engineering in Morong Rizal! We are a team of undergraduate computer
-            engineering students from the University of Rizal System in Morong
-            Rizal, Philippines. Our goal is to provide a user-friendly and
-            efficient system for managing thesis abstract submissions for the
-            College of Engineering.
-          </p>
-          <p>
-            Our mission is to streamline the thesis submission process for
-            students and faculty. We understand that submitting a thesis can be
-            a daunting and time-consuming task, which is why we developed our
-            Thesis Abstract Management System to simplify the process and reduce
-            errors.
-          </p>
-          <p>
-            The development of our Thesis Abstract Management System began as a
-            response to the challenges faced by students and faculty in managing
-            thesis abstract submissions. We are proud to have achieved several
-            milestones along the way, including the successful completion of the
-            system's prototype and beta testing.
-          </p>
-          <p>
-            Our team is composed of dedicated computer engineering students with
-            expertise in software development and programming. We are passionate
-            about creating innovative solutions to complex problems.
-          </p>
-          <p>
-            Our system's key features include a user-friendly interface, an
-            easy-to-use submission process, and a real-time tracking system.
-            These features allow students and faculty to submit and manage their
-            thesis abstracts with ease and transparency.
-          </p>
-          <p>
+    <>
+      <section className="md:pt-40 md:grid md:place-content-center">
+        <div className="text-white md:grid md:grid-cols-2 place-items-center md:h-[30em]">
+          <div className="relative w-full max-w-sm h-96 md:order-2 m-auto lg:h-[30em] lg:max-w-lg">
+            <Image
+              style={{ objectFit: "cover" }}
+              src={"/book.svg"}
+              alt="book"
+              fill
+            />
+          </div>
+          <div className="md:order-1 md:p-10">
+            <div className="md:max-w-[43em]">
+              <h1 className="text-center md:text-left my-4 text-[1.4rem] md:text-[2rem] md:max-w-lg">
+                THESIS ABSTRACT MANAGEMENT SYSTEM FOR COLLEGE OF ENGINEERING
+              </h1>
+              <p className="opacity-80 text-center md:text-left">
+                Welcome to our Thesis Abstract Management System for the College
+                of Engineering in Morong Rizal! We are a team of undergraduate
+                computer engineering students from the University of Rizal
+                System in Morong Rizal, Philippines.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="pt-32 md:pt-32 md:grid md:grid-cols-2 md:place-items-center">
+          <div className="relative h-80 w-full m-auto max-w-sm lg:h-[32em] lg:max-w-lg">
+            <Image
+              style={{ objectFit: "cover" }}
+              src={"/goals.svg"}
+              alt="goal"
+              fill
+            />
+          </div>
+          <div className="lg:px-20 md:p-10">
+            <h1 className="text-center my-4 opacity-80 md:text-left">
+              WHAT GOALS WE INTENDED TO DO?
+            </h1>
+            <p className="opacity-80 text-center md:text-left">
+              Our mission is to streamline the thesis submission process for
+              students and faculty. We understand that submitting a thesis can
+              be a daunting and timeconsuming task, which is why we developed
+              our Thesis Abstract Management System to simplify the process and
+              reduce errors.
+            </p>
+          </div>
+        </div>
+        <div className="pt-32 md:pt-0 md:grid md:grid-cols-2 md:place-items-center lg:h-[35em]">
+          <div className="relative h-72 w-full m-auto max-w-sm lg:max-w-lg order-2 lg:h-96">
+            <Image
+              style={{ objectFit: "cover" }}
+              src={"/puzzle.svg"}
+              alt="puzzle"
+              fill
+            />
+          </div>
+          <div className="lg:px-20 order-1 md:p-10">
+            <h1 className="opacity-80 text-center py-5 md:text-right">
+              TENACITY
+            </h1>
+            <p className="opacity-80 text-center md:text-right">
+              The development of our Thesis Abstract Management System began as
+              a response to the challenges faced by students and faculty in
+              managing thesis abstract submissions. We are proud to have
+              achieved several milestones along the way, including the
+              successful completion of the system's prototype and beta testing.
+            </p>
+          </div>
+        </div>
+        <div className="pt-32 md:grid md:grid-cols-2 max-w-4xl m-auto">
+          <div className="bg-slate-100 shadow-md p-10 rounded-lg m-5 text-justify">
             By using our system, students and faculty will benefit from improved
             efficiency, reduced errors, and increased transparency. In addition,
             our Thesis Abstract Management System also provides a platform for
             real-time feedback and support.
+          </div>
+          <div className="bg-slate-100 shadow-md p-10 rounded-lg m-5 text-justify">
+            Our system's key features include a userfriendly interface, an
+            easy-to-use submission process, and a real-time tracking system.
+            These features allow students and faculty to submit and manage their
+            thesis abstracts with ease and transparency.
+          </div>
+        </div>
+        <div className="pt-32 md:grid md:grid-cols-2 max-w-5xl m-auto gap-5">
+          <div className="px-5 max-w-sm m-auto">
+            <h1 className="my-4 opacity-80">GET IN TOUCH</h1>
+            <p className="opacity-80 text-justify">
+              If you have any questions or need technical support, please don't
+              hesitate to contact our team. You can find our contact information
+              on our website.
+            </p>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-3 mt-5">
+                <AiOutlinePhone color="#38649C" size={"1.5em"} />
+                torreschristian@gmail.com
+              </div>
+              <div className="flex items-center gap-3 mt-2">
+                <AiOutlineMail color="#38649C" size={"1.5em"} />
+                09683680391
+              </div>
+              <div className="flex items-center gap-3 mt-2">
+                <IoLocationOutline color="#38649C" size={"1.5em"} />
+                URS MORONG
+              </div>
+            </div>
+          </div>
+          <div className="grid place-content-center w-full">
+            <div className="bg-slate-100 mx-5 w-full shadow-md rounded-xl p-5 max-w-sm lg:absolute md:w-full md:max-w-sm md:z-10">
+              <h3 className="py-5 opacity-80">Direct Message</h3>
+              <Form>
+                <Form.Item>
+                  <Input placeholder="name" />
+                </Form.Item>
+                <Form.Item>
+                  <Input placeholder="email" />
+                </Form.Item>
+                <Form.Item>
+                  <Input.TextArea
+                    autoSize={{ minRows: 8 }}
+                    placeholder="message... "
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <PriButton>SEND</PriButton>
+                </Form.Item>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="mt-40">
+        <div className="text-white p-2 bg-[#38649C]">
+          <h1 className="text-center py-10 opacity-80">RESEARCHER</h1>
+          <div className="grid grid-cols-2 gap-5 md:grid-cols-4 max-w-3xl m-auto">
+            <div className="flex flex-col gap-2 place-items-center">
+              <Avatar className="h-28 w-28" size={"large"} />
+              <div>Christian Bert Torres</div>
+            </div>
+            <div className="flex flex-col gap-2 place-items-center">
+              <Avatar className="h-28 w-28" size={"large"} />
+              <div>Christian Bert Torres</div>
+            </div>
+            <div className="flex flex-col gap-2 place-items-center">
+              <Avatar className="h-28 w-28" size={"large"} />
+              <div>Christian Bert Torres</div>
+            </div>
+            <div className="flex flex-col gap-2 place-items-center">
+              <Avatar className="h-28 w-28" size={"large"} />
+              <div>Christian Bert Torres</div>
+            </div>
+          </div>
+          <p className="text-center py-10 opacity-80 max-w-lg m-auto">
+            Our team is composed of dedicated computer engineering students with
+            expertise in software development and programming. We are passionate
+            about creating innovative solutions to complex problems.
           </p>
-          <p>
-            Don't just take our word for it, here's what some of our users have
-            to say: [insert testimonials from users]
-          </p>
-          <p>
-            If you have any questions or need technical support, please don't
-            hesitate to contact our team. You can find our contact information
-            on our website.
-          </p>
-          <p>
-            We invite you to try our Thesis Abstract Management System for the
-            College of Engineering in Morong Rizal. Sign up today and experience
-            a streamlined and efficient thesis submission process.
-          </p>
-        </Paragraph>
-      </Row>
-      <ContactUs />
-    </section>
+        </div>
+      </div>
+    </>
   );
 };
 

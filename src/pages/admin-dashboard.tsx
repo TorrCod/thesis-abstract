8b5d19e97c6d3421ae7cb5072b08715e70e38b6c
Matsumitsu03 <matsumitsu173@gmail.com>
2023-03-04 15:51:52 +0800

    Updated the dashboard

--- src/pages/admin-dashboard.tsx
+++ src/pages/admin-dashboard.tsx
@@ -1,48 +1,49 @@
+import React from 'react';
 import { Layout, Menu } from 'antd';
-const { Header, Content, Sider } = Layout;
+const { Header, Content } = Layout;
+const { SubMenu } = Menu;
 
-function Dashboard({children}:{children:React.ReactNode}) {
+interface DashboardProps {
+  children: React.ReactNode;
+  selectedMenu: string;
+  onMenuSelect: (key: string) => void;
+}
+
+function Dashboard({ children, selectedMenu, onMenuSelect }: DashboardProps) {
   return (
-    <Layout style={{background: 'transparent'}}>
+    <Layout style={{ background: 'transparent' }}>
       <Header className="header">
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">Dashboard</Menu.Item>
-          <Menu.Item key="2">Settings</Menu.Item>
+        <Menu theme="dark" mode="horizontal" selectedKeys={[selectedMenu]} onSelect={(info) => onMenuSelect(info.key as string)} defaultSelectedKeys={['dashboard']}>
+          <Menu.Item key="dashboard" title="Dashboard">
+            Dashboard
+          </Menu.Item>
+          <Menu.Item key="settings" title="Settings">
+            Settings
+          </Menu.Item>
         </Menu>
       </Header>
       <Layout>
-        <Sider width={200}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}
-          >
-            <Menu.SubMenu key="sub1" title="Menu 1">
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-              <Menu.Item key="3">Option 3</Menu.Item>
-              <Menu.Item key="4">Option 4</Menu.Item>
-            </Menu.SubMenu>
-            <Menu.SubMenu key="sub2" title="Menu 2">
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-              <Menu.Item key="7">Option 7</Menu.Item>
-              <Menu.Item key="8">Option 8</Menu.Item>
-            </Menu.SubMenu>
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Content 
+        <Layout style={{ padding: '0 24px 24px', background: 'transparent' }}>
+          <Content
             style={{
               padding: 24,
               margin: 0,
-              minHeight: 280,
-              
+              minHeight: 900,
             }}
           >
-            {children}
+            {selectedMenu === 'dashboard' && (
+              <>
+                <div>Dashboard content</div>
+                {children}
+              </>
+            )}
+            {selectedMenu === 'settings' && (
+              <>
+                <div>Settings content</div>
+                {children}
+              </>
+            )}
           </Content>
         </Layout>
       </Layout>

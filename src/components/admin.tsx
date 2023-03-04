cf5ee35bdfdae22f7b1ce4eebe85a8b915172706
torrcod <torreschristian811@gmail.com>
2023-03-04 21:48:06 +0800

    default profile added

--- src/components/admin.tsx
+++ src/components/admin.tsx
@@ -1,7 +1,14 @@
+import { UserDetails } from "@/context/types.d";
+import { Avatar } from "antd";
 import React from "react";
 
-function AdminProfile() {
-  return <div>AdminProfile</div>;
+function AdminProfile({ userDetails }: { userDetails: UserDetails }) {
+  return (
+    <Avatar
+      className="h-12 w-12"
+      src={userDetails["profilePic"] ?? "/default-profile.png"}
+    />
+  );
 }
 
 export default AdminProfile;
--- src/components/signin_signup.tsx
+++ src/components/signin_signup.tsx
@@ -5,6 +5,7 @@ import { Course, UserDetails } from "@/context/types.d";
 import { signIn } from "@/lib/firebase";
 import useUserContext from "@/context/userContext";
 import Link from "next/link";
+import AdminProfile from "./admin";
 // import { signIn, signUp } from "@/lib/firebase";
 
 const SignInSignUp = () => {
@@ -49,9 +50,9 @@ const SignInSignUp = () => {
       setOpen(false); // close the modal after successful sign in/signup
       formSignUp.resetFields();
       message.success({
-      type: "success",
-      content:
-        "Registered Successfully! Please wait for the admins approval.",
+        type: "success",
+        content:
+          "Registered Successfully! Please wait for the admins approval.",
       });
     } catch (error) {
       const errmessage = (error as any).message;
@@ -60,7 +61,6 @@ const SignInSignUp = () => {
       }
       console.error(error);
     }
-    
   };
 
   const handleCancel = () => {
@@ -77,20 +77,23 @@ const SignInSignUp = () => {
     { value: "Electrical Engineer", label: "Electrical Engineer" },
     { value: "Mechanical Engineer", label: "Mechanical Engineer" },
   ];
-    
+
   return (
     <>
-      {userCtx.state.userDetails?<PriButton type="primary" onClick={showModal}>
-        <Link href="admin-dashboard">Open Dashboard</Link>
-      </PriButton>:<PriButton type="primary" onClick={showModal}>
-        Sign In/Sign Up
-      </PriButton>}
+      {userCtx.state.userDetails ? (
+        <AdminProfile userDetails={userCtx.state.userDetails} />
+      ) : (
+        <PriButton type="primary" onClick={showModal}>
+          Sign In / Sign Up
+        </PriButton>
+      )}
       <Modal
         centered
         bodyStyle={{ padding: "2em" }}
         open={open}
         onCancel={handleCancel}
-        footer={null}
+        okButtonProps={{ hidden: true }}
+        cancelButtonProps={{ hidden: true }}
       >
         <h3 className="text-center my-5 text-[#38649C]">Sign In</h3>
         <Form form={formSignIn} layout="vertical">
@@ -178,6 +181,10 @@ const SignInSignUp = () => {
             <PriButton onClick={handleSignUp}>Sign Up</PriButton>
           </Form.Item>
         </Form>
+        <Divider />
+        <div className="text-center opacity-80">
+          This Feature is for admin only
+        </div>
       </Modal>
     </>
   );

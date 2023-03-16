6bd9d1124e81fa3e3d6db423329d64470e17db3f
torrcod <torreschristian811@gmail.com>
2023-03-16 17:48:27 +0800

    log cleanup

--- src/lib/node_pdf.tsx
+++ src/lib/node_pdf.tsx
@@ -4,21 +4,16 @@ export const extractFromScannedImage = (
 ) => {
   const path = require("path");
   const pdf_extract = require("pdf-extract");
-
-  console.log("Usage: node thisfile.js the/path/tothe.pdf");
   const absolute_path_to_pdf = path.resolve(filePath);
   if (absolute_path_to_pdf.includes(" "))
     throw new Error(
       "will fail for paths with spaces like " + absolute_path_to_pdf
     );
-
   const options = {
     type: "ocr", // perform ocr to get the text within the scanned image
     ocr_flags: ["--psm 1"], // automatically detect page orientation
   };
-  const processor = pdf_extract(absolute_path_to_pdf, options, () =>
-    console.log("Starting…")
-  );
+  const processor = pdf_extract(absolute_path_to_pdf, options);
   processor.on("complete", (data: any) => callback(null, data));
   processor.on("error", callback);
 };

3c7131cea4753bb72c9cb7f59525df3063846edc
torrcod <torreschristian811@gmail.com>
2023-03-19 15:40:13 +0800

    fixed bug

--- src/pages/api/socket.tsx
+++ src/pages/api/socket.tsx
@@ -3,15 +3,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Server, Socket } from "socket.io";
 
 const SocketHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if ((res.socket as any).server.io) res.end();
-  const io = new Server((res.socket as any).server);
-  (res.socket as any).server.io = io;
-  watchUser((onChange) => {
-    io.emit("account-update", {
-      message: "account changed",
-      changeStream: onChange,
+  if ((res.socket as any).server.io) {
+  } else {
+    const io = new Server((res.socket as any).server);
+    (res.socket as any).server.io = io;
+
+    watchUser((onChange) => {
+      io.emit("account-update", {
+        message: "account changed",
+        changeStream: onChange,
+      });
     });
-  });
+  }
   res.end();
 };
 

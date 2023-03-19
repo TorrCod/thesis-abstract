f3330d86a8032ee434a6723e00ccbe744e2a8b8c
torrcod <torreschristian811@gmail.com>
2023-03-19 21:20:53 +0800

    case post added

--- src/pages/api/recycled-thesis.tsx
+++ src/pages/api/recycled-thesis.tsx
@@ -1,5 +1,5 @@
 import { ThesisItems } from "@/context/types.d";
-import { generateId, getData, isAuthenticated } from "@/lib/mongo";
+import { addData, generateId, getData, isAuthenticated } from "@/lib/mongo";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -11,9 +11,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!(await isAuthenticated(uid!)))
       res.status(400).json({ message: "User not found" });
 
-    const items = (await getData("thesis-abstract", "deleted-thesis")) as any[];
-    const itemsList: ThesisItems[] = generateId(items);
-    return res.status(200).json(itemsList);
+    switch (req.method) {
+      case "GET": {
+        const items = (await getData(
+          "thesis-abstract",
+          "deleted-thesis"
+        )) as any[];
+        const itemsList: ThesisItems[] = generateId(items);
+        return res.status(200).json(itemsList);
+      }
+      case "POST": {
+        const items = addData("thesis-abstract", "deleted-thesis", req.body);
+        return res.status(200).json(items);
+      }
+    }
   } catch (e) {
     console.error(e);
     return res.status(500).json({ message: "server error" });

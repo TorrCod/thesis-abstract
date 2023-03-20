c20b46f8a3c9c0506883e031e6bb99e65b755cf1
torrcod <torreschristian811@gmail.com>
2023-03-20 12:18:37 +0800

    total count added

--- src/pages/dashboard/thesis.tsx
+++ src/pages/dashboard/thesis.tsx
@@ -10,7 +10,16 @@ import {
   restoreThesisAbstract,
   thesisToDataType,
 } from "@/utils/helper";
-import { Button, Card, Divider, Menu, message, Statistic, Table } from "antd";
+import {
+  Button,
+  Card,
+  Divider,
+  Menu,
+  message,
+  Space,
+  Statistic,
+  Table,
+} from "antd";
 import { ColumnsType } from "antd/es/table";
 import Link from "next/link";
 import { MenuProps } from "rc-menu";
@@ -53,7 +62,11 @@ const DashboardThesis = () => {
         <h3 className="opacity-80 mb-3">Dashboard {">"} Thesis</h3>
         <div className="md:grid gap-2 lg:grid-cols-2 relative w-full">
           <div className="bg-white rounded-md shadow-md pt-7 mb-2 md:mb-0">
-            <p className="ml-6 opacity-60">Course Count RadarChart</p>
+            <p className="ml-6 opacity-60">Total Thesis Abstracts</p>
+            <Space className="ml-6" direction="horizontal">
+              <BsBookFill size={"1.5em"} />
+              <h1>{globalStatate.thesisItems.length}</h1>
+            </Space>
             <div className="h-96 w-full relative overflow-auto">
               <div className="h-full w-full min-w-[32em]">
                 <ResponsiveContainer width={"99%"} height="99%">

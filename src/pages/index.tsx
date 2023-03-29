80a82da80553004d1c84583d7931cbc84d53916c
torrcod <torreschristian811@gmail.com>
2023-03-30 05:49:48 +0800

    fix maximum dep exceeded

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home(props: { query: string }) {
     return () => {
       document.body.style.overflow = "visible";
     };
-  }, [promptToSignIn, props.query]);
+  }, [props.query]);
 
   return (
     <>

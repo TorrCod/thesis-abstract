53981e6846f9c68f542b478f1fe393118a24230e
torrcod <torreschristian811@gmail.com>
2023-03-31 07:01:36 +0800

    add watchChanges on layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,17 +3,20 @@ import { UserWrapper } from "@/context/userContext";
 import AntD_Config from "@/styles/antd_config";
 import Background from "./background";
 import NavBar from "./navbar";
+import WatchChanges from "./watchChanges";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <GlobalWrapper>
       <UserWrapper>
         <AntD_Config>
-          <NavBar />
-          <main>
-            <Background />
-            {children}
-          </main>
+          <WatchChanges>
+            <NavBar />
+            <main>
+              <Background />
+              {children}
+            </main>
+          </WatchChanges>
         </AntD_Config>
       </UserWrapper>
     </GlobalWrapper>
---
+++ src/components/watchChanges.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WatchChanges = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default WatchChanges;

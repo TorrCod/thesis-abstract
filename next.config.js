aaf7d837e616656115d77711ec59e610d58abf54
torrcod <torreschristian811@gmail.com>
2023-04-03 19:20:31 +0800

    configure icons and meta

--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias['react-pdf$'] = 'react-pdf/dist/esm/entry.webpack';
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+const nextConfig = withPWA(
+  {
+    reactStrictMode: true,
+    output: 'standalone',
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.alias['react-pdf$'] = 'react-pdf/dist/esm/entry.webpack';
+      }
+      return config;
+    },
+    compiler:{
+      removeConsole:process.env.NODE_ENV === "production"
     }
-    return config;
-  },
-  compiler:{
-    removeConsole:process.env.NODE_ENV === "production"
   }
-}
+)
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,10 @@ export default function Home() {
         <title>
           Thesis Abstract Management System for College of Engineering
         </title>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </Head>
       <section>
         <header className="relative text-white md:grid md:grid-cols-2 place-items-center md:h-screen md:max-h-[70em]">

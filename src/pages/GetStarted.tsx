c2c421629eff2e59f11e0d926cf461c070994e5d
Matsumitsu03 <matsumitsu173@gmail.com>
2023-04-12 19:05:48 +0800

    GetStarted_Done Suggestion: Add a Footer for Copyright Claim

--- src/pages/GetStarted.tsx
+++ src/pages/GetStarted.tsx
@@ -18,6 +18,10 @@ const styles: { [key: string]: CSSProperties } = {
     alignContent: "center",
   },
   block: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: "#F5E5E5",
     height: "400px",
     width: "600px",
@@ -25,10 +29,14 @@ const styles: { [key: string]: CSSProperties } = {
     boxShadow: "0px 10px 10px rgba(0, 0, 0, 0.5)",
     padding: "20px",
   },
+  image: {
+    height: '250px',
+  },
   //First to Last steps
   first: {
     gridColumn: "1 / span 2",
     gridRow: "2",
+    
   },
   second: {
     gridColumn: "2 / span 2",
@@ -46,6 +54,9 @@ const styles: { [key: string]: CSSProperties } = {
     gridColumn: "1 / span 2",
     gridRow: "6",
   },
+  h1: {
+    borderBottom: "1px solid black",
+  }
 };
 
 function GetStarted() {
@@ -56,49 +67,55 @@ function GetStarted() {
           Step by step guide on how to use our application and how it works:
         </div>
         <div style={{ ...styles.block, ...styles.first }}>
-          <h1>Step 1: Enter Your Search Terms</h1>
+          <h1 style={styles.h1}>Step 1: Enter Your Search Terms</h1>
           <p>
             Once you have accessed the website, you will see a search bar. Enter
             the keywords or phrases that you want to search for in this bar.
           </p>
+          <img src="/1-Step.jpg" alt="Step 1" style={styles.image}/>
         </div>
         <div style={{ ...styles.block, ...styles.second }}>
-          <h1>Step 2: Refine your Search</h1>
+          <h1 style={styles.h1}>Step 2: Refine your Search</h1>
           <p>
             If you have a large number of results, you may want to refine your
             search by using the filters provided on the website. These filters
             may include the course and the year of publication.
           </p>
+          <img src="/2-Step.jpg" alt="Step 2" style={styles.image}/>
         </div>
         <div style={{ ...styles.block, ...styles.third }}>
-          <h1>Step 3: View Your Results</h1>
+          <h1 style={styles.h1}>Step 3: View Your Results</h1>
           <p>
             Once you have entered your search terms and applied any necessary
             filters, click on the &quot;Search&quot; button to view your
             results. You will see a list of theses that match your search
             criteria.
           </p>
+          <img src="/3-Step.jpg" alt="Step 3" style={styles.image}/>
         </div>
         <div style={{ ...styles.block, ...styles.fourth }}>
-          <h1>Step 4: View Thesis Details</h1>
+          <h1 style={styles.h1}>Step 4: View Thesis Details</h1>
           <p>
             To view more information about a particular thesis, click on the
             title of the thesis. This will take you to a page with more details
             about the thesis, including the author&apos;s name, the abstract,
             and the publication information.
           </p>
+          <img src="/4-Step.jpg" alt="Step 4" style={styles.image} />
         </div>
         <div style={{ ...styles.block, ...styles.fifth }}>
-          <h1>Step 5: Download the PDF file</h1>
+          <h1 style={styles.h1}>Step 5: Download the PDF file</h1>
           <p>
             If you want to download the full text of a thesis, check if it is
             available for download. If it is, simply click on the download
             button to get a copy of the thesis.
           </p>
+          <img src="/5-Step.jpg" alt="Step 5" style={styles.image}/>
         </div>
       </div>
     </>
   );
+
 }
 
 export default GetStarted;

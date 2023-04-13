8a3f80b4531a362cacd51be524a5a50da87c8d85
torrcod <torreschristian811@gmail.com>
2023-04-13 22:32:42 +0800

    pdf docs cleanups and update storage rules

--- src/components/pdfDocs.tsx
+++ src/components/pdfDocs.tsx
@@ -71,65 +71,22 @@ const MyDocument = (props: ThesisItems) => (
   <Document>
     <Page size="LETTER" style={styles.page}>
       {props.abstract.map((url, index) => (
-        <Image key={index} src={url} />
+        // <Image key={index} src={url} />
+        <Image
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            objectFit: "fill",
+          }}
+          source={url}
+          key={index}
+        />
       ))}
     </Page>
   </Document>
-  // <Document>
-  //   <Page size="LETTER" style={styles.page}>
-  //     <View style={{ textAlign: "center" }}>
-  //       <Text>ABSTRACT</Text>
-  //     </View>
-  //     <View style={styles.twoColumn}>
-  //       <View style={styles.twoColumn1Item}>
-  //         <Text>Title:</Text>
-  //       </View>
-  //       <View style={styles.twoColumn2Item}>
-  //         <Text style={{ flex: 1, flexWrap: "wrap" }}>{props.title}</Text>
-  //       </View>
-  //     </View>
-  //     <View style={styles.twoColumn}>
-  //       <View style={styles.twoColumn1Item}>
-  //         <Text>Year:</Text>
-  //       </View>
-  //       <View style={styles.twoColumn2Item}>
-  //         <Text>{props.year}</Text>
-  //       </View>
-  //     </View>
-  //     <View style={styles.twoColumn}>
-  //       <View style={styles.twoColumn1Item}>
-  //         <Text>Course:</Text>
-  //       </View>
-  //       <View style={styles.twoColumn2Item}>
-  //         <Text>{props.course}</Text>
-  //       </View>
-  //     </View>
-  //     <View style={{ ...styles.twoColumn }}>
-  //       <View style={styles.twoColumn1Item}>
-  //         <Text>Authors:</Text>
-  //       </View>
-  //       <View
-  //         style={{
-  //           ...styles.twoColumn2Item,
-  //           flexDirection: "column",
-  //         }}
-  //       >
-  //         {props.researchers.map((child, index) => (
-  //           <Text key={index}>{`\u2022 ${child}`}</Text>
-  //         ))}
-  //       </View>
-  //     </View>
-  //     <View style={styles.body}>
-  //       <Text
-  //         style={{
-  //           textIndent: "50px",
-  //         }}
-  //       >
-  //         {props.abstract}
-  //       </Text>
-  //     </View>
-  //   </Page>
-  // </Document>
 );
 
 const PdfDownloadLink = (props: ThesisItems) => (
